bcf7330f5100c8dc80644e49f28cb7208b4723f5
Sathish Babu <sathishbabur@hotmail.com>
2018-04-01 03:00:37 +0530

    Added the Logo

--- app/src/main/java/com/ara/reach/LoginActivity.java
+++ app/src/main/java/com/ara/reach/LoginActivity.java
@@ -1,11 +1,15 @@
 package com.ara.reach;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.ara.reach.utils.AppConstants;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,5 +23,10 @@ public class LoginActivity extends AppCompatActivity {
         AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
         Button button=(Button) findViewById(R.id.btn_login);
         button.setAnimation(buttonClick);
+    }
+
+    public void link_signup_onclick(View view) {
+        Intent intent=new Intent(this,SignUpActivity.class);
+        startActivityForResult(intent, AppConstants.SIGN_UP_ACTIVITY_CODE);
     }
 }
---
+++ app/src/main/java/com/ara/reach/utils/AppConstants.java
@@ -0,0 +1,6 @@
+package com.ara.reach.utils;
+
+public class AppConstants {
+
+    public static final int SIGN_UP_ACTIVITY_CODE=2;
+}
